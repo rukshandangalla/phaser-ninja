0ca29c07455ac12c1aee03c8710609084739a6ee
rukshand <rukshandangalla@gmail.com>
2019-11-24 06:40:01 +0530

    platform and physics added

--- src/index.js
+++ src/index.js
@@ -1,35 +1,79 @@
 import Phaser from 'phaser';
 
+import tileImg from './assets/images/ground_tile.png';
 import ninjaImg from './assets/images/ninja.png';
 import ninjaJson from './assets/images/ninja.json';
+
+import { AlignGrid } from './align.grid';
+import { Align } from './align';
 
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
-  height: 600,
+  height: 800,
   scene: {
     preload: preload,
     create: create
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
   }
 };
 
 const game = new Phaser.Game(config);
+let tileGroup;
 
 function preload() {
   this.load.atlas('ninja', ninjaImg, ninjaJson);
+  this.load.image('tile', tileImg);
 }
 
 function create() {
-  const ninja = this.add.sprite(500, 300, 'ninja');
-  ninja.setScale(0.5);
-  
-  var frameNames = this.textures.get('ninja').getFrameNames();
-  console.log(frameNames);
+  tileGroup = this.physics.add.group();
+
+  const ninja = this.physics.add.sprite(400, 100, 'ninja');
+
+  setTimeout(() => {
+    ninja.body.setSize(ninja.width, ninja.height, true);
+  }, 1000);
+
+  Align.scaleToGameW(game, ninja, 0.2);
+  // ninja.setScale(0.5);
+
+  // var frameNames = this.textures.get('ninja').getFrameNames();
+  // console.log(frameNames);
 
   makeAnims(this);
-  //ninja.play('jump');
-  ninja.play('run');
+
+  const aGrid = new AlignGrid({ scene: this, rows: 11, cols: 11, gameWidth: 800, gameHeight: 600 });
+  // aGrid.showNumbers();
+
+  makeFloor(88, 98, 'tile', aGrid, this);
+
+  // ninja.play('jump');
+  ninja.play('idle');
+
+  ninja.setGravityY(200);
+  this.physics.add.collider(ninja, tileGroup);
+}
+
+function placeBlock(pos, key, grid, context) {
+  let block = context.physics.add.sprite(0, 0, key);
+  grid.placeAtIndex(pos, block);
+  tileGroup.add(block);
+  block.setImmovable();
+
+  Align.scaleToGameW(game, block, 0.09);
+}
+
+function makeFloor(fromPos, toPos, key, grid, context) {
+  for (var i = fromPos; i < toPos + 1; i++) {
+    placeBlock(i, key, grid, context);
+  }
 }
 
 function makeAnims(context) {
@@ -69,7 +113,7 @@ function makeAnims(context) {
   });
 
   context.anims.create({
-    key: 'Idle',
+    key: 'idle',
     frames: context.anims.generateFrameNames('ninja', { start: 0, end: 8, zeroPad: 3, prefix: 'Idle__', suffix: '.png' }),
     frameRate: 8,
     repeat: -1
